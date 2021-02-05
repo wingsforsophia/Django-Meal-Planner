9cc4df07dc7657cc9ef160e66aaf5cc50190f44f
Sophia Seremetis <shseremetis@gmail.com>
2021-02-05 18:47:54 -0500

    changed model and did migrations

--- main_app/models.py
+++ main_app/models.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import User
 class Plan(models.Model):
     name = models.CharField(max_length=100)
     start = models.DateField()
+    end = models.DateField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
