0806423bdf4bfbe97b89ff7c4acee72f9ac80334
Sophia Seremetis <shseremetis@gmail.com>
2021-02-05 17:56:19 -0500

    adds get absolute url

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import date
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -11,3 +12,7 @@ class Plan(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("detail", kwargs={"plan_id": self.id})
+        
