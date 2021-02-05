81ce21e73fea2c62c593f913d39403210af9ec7e
Sophia Seremetis <shseremetis@gmail.com>
2021-02-05 17:09:31 -0500

    plan index rendering

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+# from plans.views import PlanList
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('plans/', views.plans_index, name='index'), 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .models import Plan
 
+
 # Create your views here.
 def home(request):
   return render(request, 'base.html')
 
+def plans_index(request):
+  plans = Plan.objects.all()
+  return render(request, 'plans/index.html', { 'plans': plans })
+  
