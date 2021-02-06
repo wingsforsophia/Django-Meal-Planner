76767eda167e891735784323e9d6e8bc3c39d600
Sophia Seremetis <shseremetis@gmail.com>
2021-02-06 12:07:15 -0500

    add meal model

--- main_app/models.py
+++ main_app/models.py
@@ -4,6 +4,11 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 # Create your models here.
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
 
 class Plan(models.Model):
     name = models.CharField(max_length=100)
@@ -17,3 +22,15 @@ class Plan(models.Model):
     def get_absolute_url(self):
         return reverse("detail", kwargs={"plan_id": self.id})
         
+class Feeding(models.Model):
+    date = models.DateField('meal date')
+    name = models.CharField(max_length=100)
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[2][0]
+    ) 
+    plan = models.ForeignKey(Plan, on_delete=models.CASCADE)      
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
