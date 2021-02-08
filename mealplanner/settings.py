cb7725c4d26ee8741d12aa9d8147375d7c673e72
Sophia Seremetis <shseremetis@gmail.com>
2021-02-08 16:33:23 -0500

    Config deployment

--- mealplanner/settings.py
+++ mealplanner/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import environ
+environ.Env()
+environ.Env.read_env()
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e!&e%23f#vb(adi4792w%q!v_=j8=o46&&y^*trug31yy=06a$'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +127,6 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/plans/'
 
 LOGOUT_REDIRECT_URL = '/'
+
+import django_on_heroku
+django_on_heroku.settings(locals())
